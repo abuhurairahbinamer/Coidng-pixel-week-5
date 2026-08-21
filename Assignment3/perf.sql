ca648cc7858e50d6d7f5e3dd1b072798dab6cc7a
abuhurairahbinamer <138894951+abuhurairahbinamer@users.noreply.github.com>
2026-08-21 11:51:35 +0500

    fix:uncommeted the commands

--- Assignment3/perf.sql
+++ Assignment3/perf.sql
@@ -1,42 +1,42 @@
 -- -- W1
--- CREATE INDEX idx_tasks_project_id
--- ON tasks(project_id);
+CREATE INDEX idx_tasks_project_id
+ON tasks(project_id);
 
 
 -- -- W2
--- CREATE INDEX idx_tasks_status
--- ON tasks(status);
+CREATE INDEX idx_tasks_status
+ON tasks(status);
 
--- CREATE INDEX idx_tasks_assignee_id
--- ON tasks(assignee_id);
+CREATE INDEX idx_tasks_assignee_id
+ON tasks(assignee_id);
 
--- CREATE INDEX idx_tasks_due_date
--- ON tasks(due_date);
+CREATE INDEX idx_tasks_due_date
+ON tasks(due_date);
 
 
 -- C1
 --check
 --before operation 
 
--- SELECT * FROM tasks;
--- SELECT * FROM project_members;
+SELECT * FROM tasks;
+SELECT * FROM project_members;
 
--- BEGIN;
+BEGIN;
 
--- UPDATE tasks
--- SET assignee_id = 11
--- WHERE assignee_id = 10;
+UPDATE tasks
+SET assignee_id = 11
+WHERE assignee_id = 10;
 
--- DELETE FROM project_members
--- WHERE user_id = 10  AND project_id = 10;
+DELETE FROM project_members
+WHERE user_id = 10  AND project_id = 10;
 
--- COMMIT;
+COMMIT;
 
 --check
 --after operation 
 
--- SELECT * FROM tasks;
--- SELECT * FROM project_members;
+SELECT * FROM tasks;
+SELECT * FROM project_members;
 
 
 
@@ -44,21 +44,21 @@
 --C2
 
 -- Count memberships before
--- SELECT COUNT(*) FROM project_members;
+SELECT COUNT(*) FROM project_members;
 
--- BEGIN;
+BEGIN;
 
--- UPDATE tasks
--- SET assignee_id = 11
--- WHERE assignee_id = 13;
+UPDATE tasks
+SET assignee_id = 11
+WHERE assignee_id = 13;
 
--- DELETE FROM project_members
--- WHERE user_id = 13 AND project_id = 10;
+DELETE FROM project_members
+WHERE user_id = 13 AND project_id = 10;
 
--- ROLLBACK;
+ROLLBACK;
 
--- -- Count memberships after
--- SELECT COUNT(*) FROM project_members;
+-- Count memberships after
+SELECT COUNT(*) FROM project_members;
 
 
 -- C3: EXPLAIN ANALYZE
