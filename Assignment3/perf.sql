0f0db0847b20e0c075d56358e5b6ae514b7269e7
abuhurairahbinamer <138894951+abuhurairahbinamer@users.noreply.github.com>
2026-08-21 09:19:17 +0500

    feat:c2 task done

--- Assignment3/perf.sql
+++ Assignment3/perf.sql
@@ -1,39 +1,61 @@
 -- -- W1
-CREATE INDEX idx_tasks_project_id
-ON tasks(project_id);
+-- CREATE INDEX idx_tasks_project_id
+-- ON tasks(project_id);
 
 
 -- -- W2
-CREATE INDEX idx_tasks_status
-ON tasks(status);
+-- CREATE INDEX idx_tasks_status
+-- ON tasks(status);
 
-CREATE INDEX idx_tasks_assignee_id
-ON tasks(assignee_id);
+-- CREATE INDEX idx_tasks_assignee_id
+-- ON tasks(assignee_id);
 
-CREATE INDEX idx_tasks_due_date
-ON tasks(due_date);
+-- CREATE INDEX idx_tasks_due_date
+-- ON tasks(due_date);
 
 
 -- C1
 --check
 --before operation 
 
-SELECT * FROM tasks;
-SELECT * FROM project_members;
+-- SELECT * FROM tasks;
+-- SELECT * FROM project_members;
+
+-- BEGIN;
+
+-- UPDATE tasks
+-- SET assignee_id = 11
+-- WHERE assignee_id = 10;
+
+-- DELETE FROM project_members
+-- WHERE user_id = 10  AND project_id = 10;
+
+-- COMMIT;
+
+--check
+--after operation 
+
+-- SELECT * FROM tasks;
+-- SELECT * FROM project_members;
+
+
+
+
+--C2
+
+-- Count memberships before
+SELECT COUNT(*) FROM project_members;
 
 BEGIN;
 
 UPDATE tasks
 SET assignee_id = 11
-WHERE assignee_id = 10;
+WHERE assignee_id = 13;
 
 DELETE FROM project_members
-WHERE user_id = 10  AND project_id = 10;
+WHERE user_id = 13 AND project_id = 13;
 
-COMMIT;
+ROLLBACK;
 
---check
---after operation 
-
-SELECT * FROM tasks;
-SELECT * FROM project_members;
+-- Count memberships after
+SELECT COUNT(*) FROM project_members;
